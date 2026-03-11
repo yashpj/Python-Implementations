8f519d5b7d74aac0d51cb9f3fc2dfca68eff6166
Yash Gupta <yash@Mac.lan>
2026-03-10 22:31:43 -0400

    tip103-week3-session1

---
+++ stack&queues/revealAttendeeList.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+def rev(arr):
+
+    n = len(arr)
+    index_queue = deque(range(n))
+    result = [0]*n
+
+    for i in sorted(arr):
+        result[index_queue.popleft()] = i
+        if index_queue:
+            index_queue.append(index_queue.popleft())
+
+    return result
+
+print(rev([17,13,11,2,3,5,7])) 
+print(rev([1,1000]))  
