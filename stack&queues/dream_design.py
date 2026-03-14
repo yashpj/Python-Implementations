307ce69ce23d6bebff6ba35c7fc0a26652008131
Yash Gupta <yashpj007@gmail.com>
2026-03-13 23:52:51 -0400

    incomplete

---
+++ stack&queues/dream_design.py
@@ -0,0 +1,17 @@
+
+
+
+def time_to_complete_dream_designs(design_times):
+    
+    small = 10000000
+    result = [0]*len(design_times)
+    # for i in range(len(design_times)-1,-1,-1):
+    #     if design_times[i]small:
+
+    pass
+
+
+
+print(time_to_complete_dream_designs([3, 4, 5, 2, 1, 6, 7, 3])) 
+print(time_to_complete_dream_designs([2, 3, 1, 4]))  
+print(time_to_complete_dream_designs([5, 5, 5, 5]))  
