93e53f333e308097855e4a72bad12b444f7cc897
Yash Gupta <yashpj007@gmail.com>
2026-04-04 22:14:01 -0400

    refactor

--- LinkedList/Reverse_LinkedList_II_92.py
+++ LinkedList/Reverse_LinkedList_II_92.py
@@ -1,8 +1,11 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+from typing import Optional
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], leftC: int, rightC: int) -> Optional[ListNode]:
         
